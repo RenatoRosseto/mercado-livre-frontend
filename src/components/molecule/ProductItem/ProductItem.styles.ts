a31162004c1f9ecd6bc051d224d42e2fd681238d
Renato Rosseto <renato.rosseto@totvs.com.br>
2024-06-28 15:44:50 -0300

    creating product component

---
+++ src/components/molecule/ProductItem/ProductItem.styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const ProductCard = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacings.medium};
+  padding: ${({ theme }) => theme.spacings.medium};
+  cursor: pointer;
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 18rem;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProductTitle = styled.h1`
+  font-size: ${({ theme }) => theme.font.sizes.large};
+  color: ${({ theme }) => theme.colors.textPrimary};
+  margin-top: ${({ theme }) => theme.spacings.xxlarge};
+`;
+
+export const Price = styled.div`
+  font-size: ${({ theme }) => theme.font.sizes.xlarge};
+  font-weight: ${({ theme }) => theme.font.bold};
+  color: ${({ theme }) => theme.colors.textPrimary};
+  display: flex;
+  align-items: center;
+`;
+
+export const FreeShippingIcon = styled.img`
+  margin-left: 0.8rem;
+`;
---
+++ src/components/molecule/ProductItem/ProductItem.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {
+  ProductCard,
+  ImageContainer,
+  Image,
+  InfoContainer,
+  ProductTitle,
+  Price,
+  FreeShippingIcon,
+} from './ProductItem.styles';
+
+import { Product } from 'models/product';
+import { formatCurrency } from 'utils/formatCurrency';
+
+const ProductItem: React.FC<Product> = (product: Product) => {
+  return (
+    <ProductCard key={product.id} onClick={() => console.log(product.id)}>
+      <ImageContainer>
+        <Image src={product.picture_url} alt={product.title} />
+      </ImageContainer>
+      <InfoContainer>
+        <Price>
+          {formatCurrency(
+            Number(`${product.price.amount}.${product.price.decimals}`),
+            product.price.currency,
+          )}
+          {product.free_shipping && (
+            <FreeShippingIcon
+              src="/images/icons/icon-free-shipping.png"
+              alt="Ícone de caminhão indicando frete grátis"
+            />
+          )}
+        </Price>
+        <ProductTitle>{product.title}</ProductTitle>
+      </InfoContainer>
+    </ProductCard>
+  );
+};
+
+export default ProductItem;
